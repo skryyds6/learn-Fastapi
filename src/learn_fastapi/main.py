cd33fbde5121ccfcb78fdbd99a06234f955c8eba
skryyds6 <3287064984@qq.com>
2026-09-14 14:48:39 +0800

    请求体参数

--- src/learn_fastapi/main.py
+++ src/learn_fastapi/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,15 +7,10 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str | None):
-    return {"item_id": item_id, "q": q}
+class User(BaseModel):
+    username: str
+    password: str
 
-@app.get("/books/{book_id}")
-async def read_book(book_id: int = Path(..., description="The ID of the book to get", gt= 0, lt=1000)):
-    return {"book_id": book_id, "message": "Book ID must be greater than 0 and less than 1000"}
-
-@app.get("/news/new_list")
-async def read_news_list(skip: int = Query(0, min_length=0,max_length=100, description="Number of items to skip"),
-                          limit: int = Query(10, description="Maximum number of items to return")):
-    return {"skip": skip, "limit": limit, "message": "This endpoint returns a list of news articles with pagination."}
+@app.post("/register")
+async def register_user(user: User):
+    return {"message": "User registered successfully", "user": user}
