fc864d0e7d8d7c64ebeb14043b018f62582f3a86
skryyds6 <3287064984@qq.com>
2026-09-14 15:43:34 +0800

    HTMLResponse

--- src/learn_fastapi/main.py
+++ src/learn_fastapi/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -7,9 +7,6 @@ app = FastAPI()
 async def read_root():
     return {"Hello": "World"}
 
-class User(BaseModel):
-    username: str = Field(default="mike", min_length=2, max_length=10)
-    password: str = Field(default="123456", min_length=6, max_length=20)
-
-async def register_user(user: User):
-    return {"message": "User registered successfully", "user": user}
+@app.get("/html", response_class=HTMLResponse)
+async def read_html():
+    return "<h1>Hello, HTML!</h1>"
