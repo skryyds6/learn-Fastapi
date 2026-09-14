5b341b3d4cf1fbb427d9e7917e056fb6cb43019d
skryyds6 <3287064984@qq.com>
2026-09-14 14:42:20 +0800

    查询参数+Query类型注解

--- src/learn_fastapi/main.py
+++ src/learn_fastapi/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
@@ -13,3 +13,8 @@ async def read_item(item_id: int, q: str | None):
 @app.get("/books/{book_id}")
 async def read_book(book_id: int = Path(..., description="The ID of the book to get", gt= 0, lt=1000)):
     return {"book_id": book_id, "message": "Book ID must be greater than 0 and less than 1000"}
+
+@app.get("/news/new_list")
+async def read_news_list(skip: int = Query(0, min_length=0,max_length=100, description="Number of items to skip"),
+                          limit: int = Query(10, description="Maximum number of items to return")):
+    return {"skip": skip, "limit": limit, "message": "This endpoint returns a list of news articles with pagination."}
